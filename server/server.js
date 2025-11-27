51c75cca8704b5e007be68b932fa70fbb06bc5c2
Yogesh01010 <yogeshsharm26879@gmail.com>
2025-11-27 22:24:48 +0530

    svix version change

--- server/server.js
+++ server/server.js
@@ -15,15 +15,16 @@ await connectDB();
 
 //Middlewares
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) 
 
 //Routes
 app.get('/',(req,res)=> res.send("API Working"))
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
-app.post('/webhooks',clerkWebhooks)
 
+app.post('/webhooks',clerkWebhooks)
+ 
 //Port
 const PORT = process.env.PORT || 5001
 
@@ -32,4 +33,4 @@ Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server is runing on port ${PORT}`)
-})
+}) 
